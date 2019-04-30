6c18ca3d5d7bf01d81601faef73a8ef84b6bf030
上野清雅 <seiga.ueno@gmail.com>
2019-04-30 21:55:49 +0900

    new file

--- day5/counter.cpp
+++ day5/counter.cpp
@@ -1,17 +1,20 @@
 #include "counter.h"
 
-Count::Count() : m_count(0){   
+Counter::Counter() : m_count(0){   
 }
 
-int Count::m_totalCount = 0;
+int Counter::m_totalCount = 0;
 
-void Count::reset(){
+void Counter::reset(){
     m_totalCount -= m_count;
     m_count = 0;
 }
 
-void Count::count(){
+void Counter::count(){
     m_count++;
     m_totalCount++;
 }
 
+int Counter::getTotalCount(){
+  return m_totalCount;
+}
--- day5/main3.cpp
+++ day5/main3.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main()
 {
-    Count c1,c2,c3;
+    Counter c1,c2,c3;
     c1.count();
     c2.count();
     c3.count();
-    cout << Count::getTotalCount() << endl;
+    cout << Counter::getTotalCount() << endl;
     c3.count();
     c3.count();
-    cout << Count::get
+    cout << Counter::getTotalCount() << endl;
 }
